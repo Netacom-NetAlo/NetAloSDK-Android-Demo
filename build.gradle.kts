ec3af0dcc35d9f07ef2b86e250841ad6521a2f3d
Huỳnh Văn Toàn <huynhvantoan.itc@gmail.com>
2023-02-03 14:55:26 +0700

    f

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
