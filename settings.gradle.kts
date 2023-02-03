7d16c9cb5cc958e0d31394bc8177b9f2165bb292
Huỳnh Văn Toàn <huynhvantoan.itc@gmail.com>
2023-02-03 11:55:13 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ dependencyResolutionManagement {
         mavenLocal()
         maven(url = "https://jitpack.io")
         maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://github.com/Netacom-NetAlo/NetaloSDKs-Android/raw/main/ChatSDK")
     }
 }
 rootProject.name = "NetAloSDKAndroid"
